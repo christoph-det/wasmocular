c28e25385f0edb646b95c1953f91ca91a55cdae4
Christoph <christoph.dethloff@student.tuwien.ac.at>
2025-11-18 19:29:20 +0100

    added StackedAreaChart code from Binocular project

---
+++ src/components/vizualisation/cryptoUtils.ts
@@ -0,0 +1,19 @@
+/* Source: https://github.com/INSO-World/Binocular/blob/main/binocular-frontend/src/utils/crypto-utils.ts
+Accessed: 2025-11-18
+*/
+
+
+const token = 'Binocular';
+
+export const hash = async (value: any) => {
+  'use strict';
+
+  const enc = new TextEncoder();
+  const algorithm = { name: 'HMAC', hash: 'SHA-256' };
+
+  const key = await crypto.subtle.importKey('raw', enc.encode(token), algorithm, false, ['sign', 'verify']);
+  const signature = await crypto.subtle.sign(algorithm.name, key, enc.encode(value));
+  const digest = btoa(String.fromCharCode(...new Uint8Array(signature)));
+
+  return digest;
+};
