a4afe3c573f95a38d0cc7a48594d3e0b49a81841
Christoph <christoph.dethloff@student.tuwien.ac.at>
2025-11-26 14:13:36 +0100

    added chart type TextDisplay for dashboard

---
+++ src/components/vizualisation/TextDisplay.tsx
@@ -0,0 +1,25 @@
+const TextDisplay = ({ data, error }: { data: Object[]; error: string | null }) => {
+  return (data.length === 0) ? (<div>No data available: {error}</div>
+  ) : (
+    <table className="w-full table-auto border-collapse border border-gray-300">
+      <thead>
+        <tr>
+          {Object.keys(data[0]).map((key) => (
+            <th key={key} className="border border-gray-300 px-4 py-2 bg-gray-100 text-left">{key}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex} className="bg-white">
+            {Object.values(row).map((value, colIndex) => (
+              <td key={colIndex} className="border border-gray-300 px-4 py-2">{String(value)}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>  
+  );
+};
+
+export default TextDisplay
