1628f011ed42f7f7e585b5a80df1b04a809e8534
Christoph <christoph.dethloff@student.tuwien.ac.at>
2026-02-13 23:22:19 +0100

    added notice to dashboard for sql examples

--- src/pages/ExplorePageDashboard.tsx
+++ src/pages/ExplorePageDashboard.tsx
@@ -4,12 +4,14 @@ import ChartCard from "@/components/ChartCard";
 
 import { useStores } from "@/store/StoreContext";
 import { observer } from "mobx-react-lite";
+import { Link } from "react-router-dom";
 
 /**
  * ExplorePageDashboard component renders the dashboard page (with widgets) and navigation bar,
  */
 const ExplorePageDashboard = observer(() => {
   const dashboardStore = useStores().dashboardStore;
+  const widgets = dashboardStore.activeDashboard?.widgets ?? [];
 
   return (
     <div className="mx-0 bg-gradient-to-br from-gray-50 to-gray-200 min-h-screen">
@@ -19,11 +21,21 @@ const ExplorePageDashboard = observer(() => {
 
         <div className="flex-1 p-5">
           <div className="mx-auto">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
-              {dashboardStore.activeDashboard?.widgets.map((widget) => (
-                <ChartCard key={widget.id} dashboardElement={widget} />
-              ))}
-            </div>
+            {widgets.length === 0 ? (
+              <div>
+                Need some inspiration? Copy an example query from{" "}
+                <Link to="/sql-examples" className="text-blue-600 hover:underline">
+                  here
+                </Link>{" "}
+                and create your first chart.
+              </div>
+            ) : (
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+                {widgets.map((widget) => (
+                  <ChartCard key={widget.id} dashboardElement={widget} />
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </div>
